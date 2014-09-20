86d6bb4c8bf0a6eec55e8a98d351a11a6ee4725c
Tue-Cuong Dong-Si <dongsi.tuecuong@gmail.com>
2014-09-20 00:59:05 -0700

    Implement getCurrentMonthName() with the enum classes.

--- util/DateUtil.h
+++ util/DateUtil.h
@@ -10,12 +10,46 @@
 
 namespace my { namespace util {
 
-// TODO: define Month enum
-// TODO: define DayOfWeek enum
+// Month enumeration
+// NOT using enum class in C+11 for backward compatability
+struct Month
+{
+  enum type
+  {
+	JANUARY = 1,
+	FEBRUARY = 2,
+	MARCH = 3,
+	APRIL = 4,
+	MAY = 5,
+	JUNE = 6,
+	JULY = 7,
+	AUGUST = 8,
+	SEPTEMBER = 9,
+	OCTOBER = 10,
+	NOVEMBER = 11,
+	DECEMBER = 12
+  };
+};
+
+// Day of Week enumeration
+// NOT using enum class in C+11 for backward compatability
+struct DayOfWeek
+{
+  enum type
+  {
+	SUNDAY,
+	MONDAY,
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY
+  };
+};
 
 class DateTimeUtil {
 public:
-  /*
+  /**
   * Get today's year, month, day by using built-in standard library.
   * No Boost nor C++11 for backward compatability. 
   * Good enough for testing general date classes, especially on older systems.
@@ -23,6 +57,11 @@ public:
   static int getCurrentYear();
   static int getCurrentMonth();
   static int getCurrentDay();
+  
+  /**
+  * Similar to getCurrentMonth but enforce the limited enumeration of months.
+  */
+  static Month::type getCurrentMonthName() { return static_cast<Month::type>( getCurrentMonth() );}
 
 private:
 };
